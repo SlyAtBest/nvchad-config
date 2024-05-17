30cc985d48b7723ef8b139509ab424cbb6302f20
Mat <slyatbest@gmail.com>
2024-05-17 20:00:18 +0100

    change mappings for nvim-dap-go

--- lua/configs/nvim-dap-go.lua
+++ lua/configs/nvim-dap-go.lua
@@ -1,7 +1,6 @@
 local map = vim.keymap.set
 
--- dap
-map("n", "<leader>db", "<cmd> DapToggleBreakpoint <CR>", { desc = "Debug add breakpoint at line" })
+map("n", "<leader>bp", "<cmd> DapToggleBreakpoint <CR>", { desc = "Debug add breakpoint at line" })
 
 map("n", "<leader>dus", function()
   local widgets = require "dap.ui.widgets"
@@ -9,10 +8,10 @@ map("n", "<leader>dus", function()
   sidebar.open()
 end, { desc = "Debug open UI sidebar" })
 
-map("n", "<leader>dgt", function()
+map("n", "<leader>gdt", function()
   require("dap-go").debug_test()
 end, { desc = "Debug go test" })
 
-map("n", "<leader>dgl", function()
+map("n", "<leader>gdl", function()
   require("dap-go").debug_last()
 end, { desc = "Debug last go test" })
