e7c0695ff8c231520c2242c929300d8e0e4d69d8
Mat <slyatbest@gmail.com>
2024-07-01 09:37:22 +0100

    enable auto-reading of files to pick up changes outside of vim. Enable use of the OS clipboard

--- lua/options.lua
+++ lua/options.lua
@@ -1,6 +1,7 @@
 require "nvchad.options"
 
 -- add yours here!
-
--- local o = vim.o
+local o = vim.o
+o.autoread = true
+o.clipboard = "unnamed,unnamedplus"
 -- o.cursorlineopt ='both' -- to enable cursorline!
