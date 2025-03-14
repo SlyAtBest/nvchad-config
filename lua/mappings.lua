7516cc0ec90962a72c0d6a3fa7a7ebbd5ce5bdac
Mat <slyatbest@gmail.com>
2025-03-14 18:11:01 +0000

    Add handling for tmux navigator

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -4,8 +4,15 @@ require "nvchad.mappings"
 
 local map = vim.keymap.set
 
+-- TMUX navigator key overrides
+map("n", "<C-h>", ":TmuxNavigateLeft<CR>", { desc = "TMUX navigate left" })
+map("n", "<C-j>", ":TmuxNavigateDown<CR>", { desc = "TMUX navigate down" })
+map("n", "<C-k>", ":TmuxNavigateUp<CR>", { desc = "TMUX navigate up" })
+map("n", "<C-l>", ":TmuxNavigateRight<CR>", { desc = "TMUX navigate right" })
+
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
+map("v", "jk", "<ESC>")
 map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 map("n", "vd", "<cmd>lua vim.diagnostic.open_float()<cr>")
 map("n", "ri", function()
